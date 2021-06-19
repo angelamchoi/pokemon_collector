c8c6bc08cd56582900e9cac7d2c717d49b497ddd
Angela Choi <angelamchoi@gmail.com>
2021-06-19 18:24:11 -0400

    added feeding model and worked on details page

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse #reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+    ('S', 'Snack')
+)
+
 # pokemon model
 class Pokemon(models.Model):
     name = models.CharField(max_length =100)
@@ -15,3 +22,20 @@ class Pokemon(models.Model):
 # Method
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pokemon_id': self.id})
+
+# feeding model
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0] #default to be 'B'
+    )
+
+    #pokemon FK
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+
