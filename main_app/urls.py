b88d9cf4fc3a031888e2af8995e2f61dcb8d3bff
Angela Choi <angelamchoi@gmail.com>
2021-06-24 14:47:44 -0400

    added notes to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,20 +5,20 @@ urlpatterns =[
     path('', views.home, name='home'), #homepage
     path('about/', views.about, name='about'), #about
     path('pokemons/', views.pokemons_index, name='index'), #pokemons
-    path('pokemons/<int:pokemon_id>/', views.pokemons_detail, name='detail'), #details
-    path('pokemons/create/', views.PokemonCreate.as_view(), name ='pokemons_create'), #create CBV
+    path('pokemons/<int:pokemon_id>/', views.pokemons_detail, name='detail'), #pokemon details
+    path('pokemons/create/', views.PokemonCreate.as_view(), name ='pokemons_create'), #create pokemon CBV
     path('pokemons/<int:pk>/update/', views.PokemonUpdate.as_view(), name='pokemons_update'), #update
     path('pokemons/<int:pk>/delete/', views.PokemonDelete.as_view(), name='pokemons_delete'), #delete
     path('pokemons/<int:pokemon_id>/add_feeding/', views.add_feeding, name='add_feeding'), #feeding
-    path('toys/', views.ToyList.as_view(), name = 'toys_index'),
-    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
-    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
-    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
-    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
-    path('pokemons/<int:pokemon_id>/assoc_toy/<int:toy_id>/',views.assoc_toy, name='assoc_toy'),
-    path('pokemons/<int:pokemon_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
-    path('pokemons/<int:pokemon_id>/add_photo/', views.add_photo, name='add_photo'),
-    path('accounts/signup/', views.signup, name='signup'),
+    path('toys/', views.ToyList.as_view(), name = 'toys_index'), #toys
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'), #toy detail
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'), # create Toy CBV
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'), # toy update
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'), # toy delete
+    path('pokemons/<int:pokemon_id>/assoc_toy/<int:toy_id>/',views.assoc_toy, name='assoc_toy'), # assoc toy
+    path('pokemons/<int:pokemon_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'), #unassoc toy
+    path('pokemons/<int:pokemon_id>/add_photo/', views.add_photo, name='add_photo'), #photo
+    path('accounts/signup/', views.signup, name='signup'), #signup
 ]
 
 #### Notes #####
